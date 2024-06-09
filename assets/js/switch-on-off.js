020dd1f51ee2e2f801b2da8b207a6c8273531cb5
Hnin-Nu-Hlaing <sakurayk2002@gmail.com>
2024-06-09 14:51:49 +0630

    module-3

--- assets/js/switch-on-off.js
+++ assets/js/switch-on-off.js
@@ -1,22 +1,8 @@
 function switchOn() {
-  var light = document.getElementById("light");
-  light.style.backgroundImage = "url('./assets/images/switch-on.png')"; // switch on
+    var light = document.getElementById("light");
+    light.style.backgroundImage = "url('./assets/images/switch-on.png')"; // switch on
 }
-
 function switchOff() {
-  var light = document.getElementById("light");
-  light.style.backgroundImage = "url('./assets/images/switch-off.png')"; // switch off
+    var light = document.getElementById("light");
+    light.style.backgroundImage = "url('./assets/images/switch-off.png')"; // switch off
 }
-
-/*
-//when bg-color set to grey color, you can use this
-function switchOn() {
-  var light = document.getElementById("light");
-  light.style.backgroundColor = "yellow"; // switch on
-}
-
-function switchOff() {
-  var light = document.getElementById("light");
-  light.style.backgroundColor = "grey"; // switch off
-}
-*/
